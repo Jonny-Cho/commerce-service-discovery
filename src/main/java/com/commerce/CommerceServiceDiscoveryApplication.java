a118f42d5901b3607cd43ddccc4592a2016f4c45
Jonny-Cho <andante2183@gmail.com>
2022-01-13 21:58:33 +0900

    setting: 프로젝트 세팅

---
+++ src/main/java/com/commerce/CommerceServiceDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CommerceServiceDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceServiceDiscoveryApplication.class, args);
+    }
+
+}
